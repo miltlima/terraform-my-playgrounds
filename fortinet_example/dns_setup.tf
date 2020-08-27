bc84b77a5218a085d5db1d01fbffcdcbbf819e58
Milton Lima <milton.lima@outlook.com>
2020-08-26 22:36:30 -0300

    Example Configure a DNS using Terraform to FortIOS

---
+++ fortinet_example/dns_setup.tf
@@ -0,0 +1,23 @@
+provider "fortios" {
+  version = "~> 1.5"
+  hostname = "10.1.2.3"
+  token    = "${var.TOKEN}"
+}
+
+resource "fortios_system_setting_dns" "dnstest" {
+  primary   = "99.99.99.99"
+  secondary = "8.8.8.8"
+}
+
+resource "fortios_networking_route_static" "dnstest" {
+  dst = "83.25.12.19/32"
+  gateway = "1.1.1.1"
+  blackhole = "disable"
+  distance = "22"
+  weight = "3"
+  priority = "1"
+  device = "port1"
+  comment = "Dns demo test"
+}
+
+
