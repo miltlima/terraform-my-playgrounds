a1f63c1324dcbccadebbc86a0ea5212595f30401
Milton Lima <milton.lima@outlook.com>
2020-09-03 19:21:47 -0300

    Added improvements

--- makedir.py
+++ makedir.py
@@ -1,8 +1,13 @@
-# coding: future_fstrings
+# Name : Project Make dir
+# Author Milton Lima
+# Release:
+# Version: 0.0.1
+
 import os
+import sys
 
 path = os.getcwd()
-print(f'Voce esta no diretorio {path}')
+print(f'Voce está no diretório {path}')
 
 
 def create_dir():
@@ -10,10 +15,16 @@ def create_dir():
         name_dir = str(input("Informe o nome do projeto que deseja criar: "))
         if name_dir == '':
             continue
-        else:
-            os.mkdir(name_dir)
-            print(f"Pasta criada com sucesso {name_dir}")
-        return()
+        else :
+            try:
+                os.mkdir(name_dir)
+            except FileExistsError:
+                print('A pasta já existe')
+                continue
+        os.mkdir(name_dir)
+        print(f"Pasta criada com sucesso {name_dir}")
+        break
+    return()
 
 
 create_dir()
