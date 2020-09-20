3ef56a4f5e7aee2fac98991703c8d2f8ce015a68
Milton Lima <milton.lima@outlook.com>
2020-09-19 23:44:41 -0300

    Putting a EC2 instance in a recently created VPC

--- labs_vpc_ec2_ebs/ec2.tf
+++ labs_vpc_ec2_ebs/ec2.tf
@@ -1,6 +1,11 @@
 resource "aws" "melebesquet" {
   ami           = var.AMIS[var.REGION]
   instance_type = "t2.micro"
+  #VPC Subnet // #SG //  #SSH public key
+  subnet_id               = aws_subnet.main-public-1.id
+  vpc_security_groups_ids = [aws_security_group.allow-ssh.id]
+  key_name                = aws_key_pair.mykeypair.key_name
+
   provisioner "local-exec" {
     command = "echo ${aws_instance.melebesquet.private_ip} >> hosts.txt"
   }
--- labs_vpc_ec2_ebs/vars.tf
+++ labs_vpc_ec2_ebs/vars.tf
@@ -5,10 +5,18 @@ variable "AWS_REGION" {
 }
 
 variable "AMIS" {
-  type = map
+  type = map(string)
   default = {
     us-east-1 = "ami-0bcc094591f354be2"
     us-west-1 = "ami-05fe528ab5127b0b2"
     us-west-2 = "ami-0b685684b5f489427"
   }
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "cocokey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "cocokey.pub"
 }
