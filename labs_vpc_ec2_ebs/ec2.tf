ff04f06cf97546309f43f0bca2edfe9d21185fe8
Milton Lima <milton.lima@outlook.com>
2020-09-20 22:59:21 -0300

    Putting a EC2 instance in a recently created VPC

--- labs_vpc_ec2_ebs/ec2.tf
+++ labs_vpc_ec2_ebs/ec2.tf
@@ -4,7 +4,7 @@ resource "aws" "melebesquet" {
   #VPC Subnet // #SG //  #SSH public key
   subnet_id               = aws_subnet.main-public-1.id
   vpc_security_groups_ids = [aws_security_group.allow-ssh.id]
-  key_name                = aws_key_pair.mykeypair.key_name
+  key_name                = aws_key_pair.cocokey.key_name
 
   provisioner "local-exec" {
     command = "echo ${aws_instance.melebesquet.private_ip} >> hosts.txt"
