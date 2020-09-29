48a78d44b4943e378c602df4b86be7895c559e9c
Milton Lima <milton.lima@outlook.com>
2020-09-29 12:47:02 -0300

    Added RDS maria DB

---
+++ labs_rds/instance.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "melebesquet" {
+  ami           = var.AMIS[var.AWS_REGION]
+  instance_type = "t2.micro"
+  #VPC Subnet // #SG //  #SSH public key 
+  subnet_id              = aws_subnet.main-public-1.id
+  vpc_security_group_ids = [aws_security_group.melebesquet.id]
+  key_name               = var.PATH_TO_PRIVATE_KEY
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.melebesquet.public_ip} >> hosts.txt"
+  }
+}
